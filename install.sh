f0a3d3add911470f36d452c1f3243a3b5c212805
geometr <hq@aveslab.ru>
2024-02-20 09:06:20 +0300

    Add neovim config

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 stow -Rvt ~ bash/
+stow -Rvt ~ nvim/
---
+++ nvim/.config/nvim/lua/plugins.lua
@@ -0,0 +1,23 @@
+return require('packer').startup(function()
+  use 'wbthomason/packer.nvim'
+  use 'OmniSharp/omnisharp-vim'
+  use 'neovim/nvim-lspconfig' 
+  use 'tpope/vim-fugitive'
+  use 'neoclide/coc.nvim'
+  use 'wakatime/vim-wakatime'
+  use 'dense-analysis/ale'
+  use 'folke/tokyonight.nvim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+  use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+  use 'L3MON4D3/LuaSnip' -- Snippets plugin
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'lewis6991/gitsigns.nvim'
+  use 'ms-jpq/coq_nvim'
+  use 'folke/which-key.nvim'
+end)
+
